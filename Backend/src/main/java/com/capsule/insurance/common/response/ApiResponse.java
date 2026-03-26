588e1b44f1f3bb550a5d2e3b1bfc5e74a117c9fa
Willie <150939763+junggyo1020@users.noreply.github.com>
2026-03-26 10:57:46 +0900

    [BE] 116 chore: 공통 응답에 메시지를 포함한 data를 받는 응답형식 추가

--- Backend/src/main/java/com/capsule/insurance/common/response/ApiResponse.java
+++ Backend/src/main/java/com/capsule/insurance/common/response/ApiResponse.java
@@ -1,4 +1,3 @@
-// #Demo Setting
 package com.capsule.insurance.common.response;
 
 import com.capsule.insurance.common.exception.ErrorCode;
@@ -14,6 +13,10 @@ public record ApiResponse<T>(
 
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse<>(true, data, null, "OK", Instant.now());
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, data, null, message, Instant.now());
     }
 
     public static ApiResponse<Void> error(ErrorCode errorCode, String message) {
